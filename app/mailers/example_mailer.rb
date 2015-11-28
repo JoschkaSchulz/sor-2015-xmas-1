9019ba3ec67ebc1daaaa44a5302716cbfa51de64
Florian Schultz <florian.schultz93@web.de>
2015-11-28 18:07:13 +0100

    Senderemail geändert auf Wunschliste

--- app/mailers/example_mailer.rb
+++ app/mailers/example_mailer.rb
@@ -1,5 +1,5 @@
 class ExampleMailer < ActionMailer::Base
-  default from: "from@example.com" #Email des Benutzers, der die Wunachliste schickt
+  default from: "Wunschliste" #Email des Benutzers, der die Wunachliste schickt
 
   def send_email(recipient, sender)
     @recipient = recipient
