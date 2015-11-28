b3866edcfd53b225f461d0faab27dcdbeeee2ab0
Florian Schultz <florian.schultz93@web.de>
2015-11-28 15:35:11 +0100

    Beispiel Email erstellt

--- app/mailers/example_mailer.rb
+++ app/mailers/example_mailer.rb
@@ -1,2 +1,8 @@
-class ExampleMailer < ApplicationMailer
+class ExampleMailer < ActionMailer::Base
+  default from: "from@example.com" #Email des Benutzers, der die Wunachliste schickt
+
+  def sample_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Sample Email')
+  end
 end
