f86702b44d58e18fbfb8ed43682e9dfe32fb43b1
DanielJ <daniel.p.jaeger@gmail.com>
2015-11-29 15:00:56 +0100

    Social media buttons hinzugefügt.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'shareable'
 
 gem 'tzinfo-data'
 
---
+++ test/helpers/homes_helper_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class HomesHelperTest < ActionView::TestCase
+  def test_sanity
+    flunk "Need real tests"
+  end
+end
